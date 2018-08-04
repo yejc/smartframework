966f2704019477da5c383c331875f78fdcf11fb7
yejc <yejc1995@foxmail.com>
2018-08-04 21:56:23 +0800

    init

---
+++ src/main/java/org/smart4j/framework/ConfigConstant.java
@@ -0,0 +1,11 @@
+package org.smart4j.framework;
+
+public interface ConfigConstant {
+    String CONFIG_FILE = "smart.properties";
+    String JDBC_URL = "smart.framework.jdbc.url";
+    String JDBC_USERNAME = "smart.framework.jdbcsmart.framework.jdbc.username";
+    String JDBC_PASSWORD = "smart.framework.jdbc.password";
+    String APP_BASE_PACKAGE = "smart.framework.app.base.package";
+    String APP_JSP_PATH = "smart.framework.jdbc.app.jsp.path";
+    String APP_ASSET_PATH = "smart.framework.jdbc.app.asset_path";
+}
